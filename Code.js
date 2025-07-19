b25517b99ea69c3f9db2999f3372c82acdd897c0
AaronJonesDesigns <ajones90747@uat.edu>
2025-07-19 15:42:21 -0700

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,4 @@
+// JavaScript defining the playZeblor function and directing it to the site where Zeblor can be played.
+function playZeblor() {
+        window.location.href = "https://www.construct.net/en/free-online-games/zeblor-mystic-journey-doom-78664/play"
+    }
